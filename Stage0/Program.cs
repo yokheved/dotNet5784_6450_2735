134e8538660d4f04eda918c3e565a4628645da43
yokheved <yokheved.spr@gmail.com>
2023-10-25 21:50:35 +0300

    Initial Code + Welcome functions

---
+++ Stage0/Program.cs
@@ -0,0 +1,23 @@
+﻿partial class Program
+{
+    private static void Main(string[] args)
+    {
+        Welcome6450();
+        Welcome2735();
+        //Enter your name:
+        //yokheved
+        //yokheved, welcome to my first console application
+    }
+
+    private static void Welcome6450()
+    {
+        Console.WriteLine("Enter your name:  ");
+        string name = Console.ReadLine();
+        Console.WriteLine("{0}, welcome to my first console application", name);
+    }
+
+    private static void Welcome2735()
+    {
+
+    }
+}
