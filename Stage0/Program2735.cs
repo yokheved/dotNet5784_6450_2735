8416afd847e208137bd826baa455ac1bcd7139bd
YAEL-TAUB <yaelt5683@gmail.com>
2023-10-29 15:39:34 +0200

    Initial Code + Welcome functions

---
+++ Stage0/Program2735.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        private static void Welcome2735()
+        {
+            Console.WriteLine("I'm here!  ");
+        }
+    }
+}
