992f94e3b607e82d8faea53eebbef74dc4d563c1
yokheved <yokheved.spr@gmail.com>
2023-11-06 19:54:43 +0200

    chapter 6 - begining

---
+++ DalList/DependencyImplementation.cs
@@ -0,0 +1,32 @@
+﻿using Do;
+
+namespace Dal
+{
+    internal class DependencyImplementation : IDependency
+    {
+        public int Create(IDependency item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IDependency? Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<IDependency> ReadAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(IDependency item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DalList/TaskImplementation.cs
@@ -0,0 +1,30 @@
+﻿using DO;
+
+namespace Dal;
+public class TaskImplementation : ITask
+{
+    public int Create(ITask item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ITask? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<ITask> ReadAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(ITask item)
+    {
+        throw new NotImplementedException();
+    }
+}
