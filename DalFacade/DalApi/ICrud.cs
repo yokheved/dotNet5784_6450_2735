e8fcbc7c1d725ca0a90d3ff9f49c4ae1cfdbe238
yokheved <yokheved.spr@gmail.com>
2023-11-11 19:53:49 +0200

    stage 2 chapter 11

--- DalFacade/DalApi/ICrud.cs
+++ DalFacade/DalApi/ICrud.cs
@@ -3,31 +3,13 @@
 public interface ICrud<T> where T : class
 {
     //all commants are for type dependency, need to be changed.
-    /// <summary>
-    /// Creates new Dependency entity object in DAL
-    /// </summary>
-    /// <param name="item">Dependency type</param>
-    /// <returns>integer - new item id</returns>
+
     int Create(T item);
-    /// <summary>
-    /// Reads entity object by its ID 
-    /// </summary>
-    /// <param name="id">id of Dependency to read</param>
-    /// <returns>Dependency object with param id, if not found - null</returns>
+
     T? Read(int id);
-    /// <summary>
-    /// Reads all entity objects
-    /// </summary>
-    /// <returns> list type Dependency of all dependencies</returns>
+
     List<T> ReadAll();
-    /// <summary>
-    /// Updates entity object
-    /// </summary>
-    /// <param name="item">new item - the item with id to update, and values to update</param>
+
     void Update(T item);
-    /// <summary>
-    /// Deletes an object by its Id
-    /// </summary>
-    /// <param name="id">id of dependency to delete</param>
     void Delete(int id);
 }
--- DalList/EngineerImplementation.cs
+++ DalList/EngineerImplementation.cs
@@ -4,49 +4,66 @@ namespace Dal;
 
 internal class EngineerImplementation : IEngineer
 {
-    //function declarations and comants in IEngineer
+    /// <summary>
+    /// Creates new Engineer entity object in DAL
+    /// </summary>
+    /// <param name="item">Engineer type</param>
+    /// <returns>integer - new Engineer item id</returns>
     public int Create(Engineer item)
     {
         if (DataSource.Engineers.Contains(item))
         {
-            throw new Exception($"Object of type Engineer with ID {item.Id} exists.");
+            throw new DalAlreadyExistsException($"Object of type Engineer with ID {item.Id} exists.");
         }
         Engineer tempItem = new Engineer(item.Id, item.Name, item.Email, item.Level, item.Cost);
         DataSource.Engineers.Add(tempItem);
         return item.Id;
     }
-
+    /// <summary>
+    /// Deletes a Engineer object by its Id
+    /// </summary>
+    /// <param name="id">id of Engineer to delete</param>
     public void Delete(int id)
     {
         Engineer? deleteIt = DataSource.Engineers.Find(item => item.Id == id);
 
         if (deleteIt == null)
         {
-            throw new Exception($"Object of type Engineer with ID {id} does not exist.");
+            throw new DalDoesNotExistException($"Object of type Engineer with ID {id} does not exist.");
         }
         else
         {
             DataSource.Engineers.Remove(deleteIt);
         }
     }
-
+    /// <summary>
+    /// Reads Engineer entity object by its ID 
+    /// </summary>
+    /// <param name="id">id of Engineer to read</param>
+    /// <returns>Engineer object with param id, if not found - null</returns>
     public Engineer? Read(int id)
     {
         return DataSource.Engineers.Find(Item => Item.Id == id);
     }
-
+    /// <summary>
+    /// Reads all Engineer entity objects
+    /// </summary>
+    /// <returns> list type Engineer of all Engineers</returns>
     public List<Engineer> ReadAll()
     {
         return new List<Engineer>(DataSource.Engineers);
     }
-
+    /// <summary>
+    /// Updates Engineer entity object
+    /// </summary>
+    /// <param name="item">new Engineer item - the item with id to update, and values to update</param>
     public void Update(Engineer item)
     {
         Engineer? existingItem = DataSource.Engineers.Find(Engineer => Engineer.Id == item.Id);
 
         if (existingItem == null)
         {
-            throw new Exception($"Object of type Engineer with ID {item.Id} does not exist.");
+            throw new DalDoesNotExistException($"Object of type Engineer with ID {item.Id} does not exist.");
         }
         else
         {
