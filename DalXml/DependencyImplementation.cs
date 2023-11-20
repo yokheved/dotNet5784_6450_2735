1e351fe59bd4231f7de303b31f13fa0feb64454f
YAEL-TAUB <yaelt5683@gmail.com>
2023-11-20 17:31:13 +0200

    start stage 3

---
+++ DalXml/DependencyImplementation.cs
@@ -0,0 +1,36 @@
+﻿namespace Dal;
+using DO;
+
+
+internal class DependencyImplementation : IDependency
+{
+    public int Create(Dependency item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dependency? Read(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Dependency? Read(Func<Dependency, bool> filter)
+    {
+        throw new NotImplementedException();
+    }
+
+    public IEnumerable<Dependency> ReadAll(Func<Dependency, bool>? filter = null)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Dependency item)
+    {
+        throw new NotImplementedException();
+    }
+}
